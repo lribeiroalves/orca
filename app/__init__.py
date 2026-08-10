1fa3302a4f7c860009c9ffd85befd5f1b6096bd3
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-08-10 05:07:27 -0300

    flask app minimo possivel

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return 'hello, world!'
+
+    return app
+
+
+app = create_app()
