7b2cbe55e83b00af714ae7c9b4507f94c219fe4f
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-08-17 01:05:08 -0300

    estrutura do banco atualizada

--- app/ext/database/models.py
+++ app/ext/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Numeric, Boolean, ForeignKey, CheckConstraint, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import db
 
@@ -15,4 +15,164 @@ class Users(db.Model):
         return {
             'id': self.id,
             'nome': self.nome
+        }
+
+
+class Entradas(db.Model):
+    __table_args__ = (
+        CheckConstraint("ano >= 2026", name="check_ano_valido"),
+        CheckConstraint("mes >= 1 AND mes <= 12", name="check_mes_valido"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ano: Mapped[int] = mapped_column(nullable=False)
+    mes: Mapped[int] = mapped_column(nullable=False)
+    descricao: Mapped[str] = mapped_column(String(255), nullable=False)
+    valor: Mapped[float] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+
+    # Relacoes
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship('Users')
+
+    def __repr__(self):
+        return f'Entrada(id: {self.id}, ano: {self.ano}, mes: {self.mes}, descricao: {self.descricao}, valor: {self.valor}, user_id: {self.user_id}, user: {self.user})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ano': self.ano,
+            'mes': self.mes,
+            'descricao': self.descricao,
+            'valor': self.valor,
+            'user_id': self.user_id
+        }
+
+
+class Saidas(db.Model):
+    __table_args__ = (
+        CheckConstraint("ano >= 2026", name="check_ano_v"),
+        CheckConstraint("mes >= 1 AND mes <= 12", name="check_mes_v"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ano: Mapped[int] = mapped_column(nullable=False)
+    mes: Mapped[int] = mapped_column(nullable=False)
+    descricao: Mapped[str] = mapped_column(String(255), nullable=False)
+    valor: Mapped[float] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+
+    # Relacoes
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship('Users')
+
+    def __repr__(self):
+        return f'Saida(id: {self.id}, ano: {self.ano}, mes: {self.mes}, descricao: {self.descricao}, valor: {self.valor}, user_id: {self.user_id}, user: {self.user})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ano': self.ano,
+            'mes': self.mes,
+            'descricao': self.descricao,
+            'valor': self.valor,
+            'user_id': self.user_id
+        }
+
+
+class Bancos(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    def __repr__(self):
+        return f'Banco(id: {self.id}, nome: {self.nome})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nome': self.nome
+        }
+
+
+class Saldos(db.Model):
+    __table_args__ = (
+            CheckConstraint("ano >= 2026", name="check_ano"),
+            CheckConstraint("mes >= 1 AND mes <= 12", name="check_mes"),
+            UniqueConstraint('ano', 'mes', 'banco_id', 'user_id', name='unique_ano_mes_banco_user')
+        )
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ano: Mapped[int] = mapped_column(nullable=False)
+    mes: Mapped[int] = mapped_column(nullable=False)
+    valor: Mapped[float] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+
+    # relacoes
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship('Users')
+    banco_id: Mapped[int] = mapped_column(ForeignKey('bancos.id'))
+    banco: Mapped['Bancos'] = relationship('Bancos')
+
+    def __repr__(self):
+        return f'valor(id: {self.id}, ano: {self.ano}, mes: {self.mes}, valor: {self.valor}, user_id: {self.user_id}, user: {self.user}, banco_id: {self.banco_id}, banco: {self.banco})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ano': self.ano,
+            'mes': self.mes,
+            'valor': self.valor,
+            'user_id': self.user_id,
+            'banco_id': self.banco_id
+        }
+
+
+class Faturas(db.Model):
+    __table_args__ = (
+                CheckConstraint("ano >= 2026", name="check_ano_fatura"),
+                CheckConstraint("mes >= 1 AND mes <= 12", name="check_mes_fatura"),
+                CheckConstraint("status_paga IN (0,1)", name="check_bool"),
+            )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ano: Mapped[int] = mapped_column(nullable=False)
+    mes: Mapped[int] = mapped_column(nullable=False)
+    status_paga: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f'Fatura(id: {self.id}, ano: {self.ano}, mes: {self.mes}, status_paga: {self.status_paga})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ano': self.ano,
+            'mes': self.mes,
+            'status_paga': self.status_paga
+        }
+    
+
+
+class Compras(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    valor: Mapped[float] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+    descricao: Mapped[str] = mapped_column(String(255), nullable=False)
+    parcelas: Mapped[int] = mapped_column(nullable=False)
+
+    # relacoes
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship('Users')
+    banco_id: Mapped[int] = mapped_column(ForeignKey('bancos.id'))
+    banco: Mapped['Bancos'] = relationship('Bancos')
+    fatura_id: Mapped[int] = mapped_column(ForeignKey('faturas.id'))
+    fatura: Mapped['Faturas'] = relationship('Faturas')
+
+    def __repr__(self):
+        return f'Compra(id: {self.id}, valor: {self.valor}, descricao: {self.descricao}, parcelas: {self.parcelas}, user_id: {self.user_id}, user: {self.user}, banco_id: {self.banco_id}, banco: {self.banco}, fatura_id: {self.fatura_id}, fatura: {self.fatura})'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'valor': self.valor,
+            'descricao': self.descricao,
+            'parcelas':self.parcelas,
+            'user_id': self.user_id,
+            'banco_id': self.banco_id,
+            'fatura_id': self.fatura_id
         }
