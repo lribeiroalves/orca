29f58f38154281fd29c0e6c1656645d8b9613a22
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-08-18 00:04:20 -0300

    verificacao da versao do banco antes de rodar o migration

--- app/ext/migration/__init__.py
+++ app/ext/migration/__init__.py
@@ -1,4 +1,9 @@
 from flask_migrate import Migrate, upgrade as migrate_upgrade
+from alembic.script import ScriptDirectory
+from alembic.config import Config
+from alembic.runtime.migration import MigrationContext
+from sqlalchemy import create_engine
+
 import os
 
 from app.ext.database import db
@@ -6,16 +11,33 @@ from app.ext.database.models import *
 from app import get_base_path
 
 base_path = get_base_path()
+migrations_dir = os.path.join(base_path, 'migrations')
+
+
+def banco_atualizado(db_url):
+    alembic_cfg = Config(os.path.join(migrations_dir, 'alembic.ini'))
+    alembic_cfg.set_main_option("script_location", migrations_dir)
+    script = ScriptDirectory.from_config(alembic_cfg)
+    head = script.get_current_head()
+
+    engine = create_engine(db_url)
+    with engine.connect() as conn:
+        context = MigrationContext.configure(conn)
+        current = context.get_current_revision()
+
+    return current == head
+
 
 def init_app(app):
-    migrations_dir = os.path.join(base_path, 'migrations')
-
     migrate = Migrate(app, db, directory=migrations_dir)
 
     if os.path.exists(migrations_dir):
         with app.app_context():
             try:
-                migrate_upgrade(directory=migrations_dir)
-                print("Migrações aplicadas com sucesso.")
+                if not banco_atualizado(app.config['SQLALCHEMY_DATABASE_URI']):
+                    migrate_upgrade(directory=migrations_dir)
+                    print("Migrações aplicadas com sucesso.")
+                else:
+                    print('Banco atualizado!')
             except Exception as e:
                 print(f'Erro ao aplicar migrações: {e}')
