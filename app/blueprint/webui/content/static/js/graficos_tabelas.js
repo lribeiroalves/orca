114eb02647b66e1154361623b37324bce9ac0901
Lucas Ribeiro <lucasribeiroalves@github.no-reply.com>
2026-09-14 23:36:11 -0300

    graficos da tela tabelas funcionando

--- app/blueprint/webui/content/static/js/graficos_tabelas.js
+++ app/blueprint/webui/content/static/js/graficos_tabelas.js
@@ -20,13 +20,74 @@ $(function() {
             datasets: [{
                 label: '',
                 data: values_es,
-                backgroundColor: ['rgba(255, 255, 224, 0.6)', 'rgba(255, 182, 193, 0.6)'],
+                backgroundColor: ['rgba(200, 200, 255, 0.6)', 'rgba(255, 200, 200, 0.6)'],
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1
             }]
         },
         options: {
             responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grace: '15%',
+                    ticks: {
+                        callback: function(value) {
+                            return 'R$ ' + value.toLocaleString('pt-BR');
+                        }
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+
+                // 1. Configuração do Tooltip (Hover)
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                // Pegamos o valor exato do eixo Y
+                                let valor = context.parsed.y;
+                                return formatarReais(valor);
+                            }
+                        }
+                    },
+                    
+                    // 2. Configuração do DataLabels (Texto no topo da barra)
+                    datalabels: {
+                        anchor: 'end',  // Ancora o texto no final da barra (topo)
+                        align: 'end',   // Alinha para fora/cima da barra
+                        formatter: function(value) {
+                            return formatarReais(value);
+                        },
+                        font: {
+                            weight: 'bold'
+                        },
+                        color: '#333' // Cor do texto
+                    }
+            }
+        }
+    });
+
+
+    new Chart($('#graf-p'), {
+        type: 'bar',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: labels_p,
+            datasets: [{
+                label: '',
+                data: values_p,
+                backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
             scales: {
                 y: {
                     beginAtZero: true,
